5dbcc275f58d5a8a92ce95ee160b0026e4a423af
Merve <61613791+merveozderin@users.noreply.github.com>
2021-08-15 14:04:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı giriniz: ") ;
+            int sayac= int.Parse(Console.ReadLine()) ;
+            for (int i = 1; i < sayac; i++)
+            {
+                if(i%2==1)
+                Console.WriteLine(i) ;    
+            }
+            
+            int tektoplam=0 ;
+            int cifttoplam=0 ;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if(i%2==1)
+                tektoplam +=i ;
+
+                else
+                cifttoplam +=i ;
+            }
+
+            Console.WriteLine("Tek Toplam:" +tektoplam) ;
+            Console.WriteLine("Çift Toplam:" +cifttoplam) ;
+
+            // Break, continue komutları
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(i==4)
+                break;
+                Console.WriteLine(i) ;                
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(i==4)
+                continue;
+                Console.WriteLine(i) ;                
+            }
+
+            
+        }
+    }
+}
